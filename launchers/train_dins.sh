bdd59efaf81f57550967b97bd69fe0059dfd6be9
GeorgBell <georgbell5@gmail.com>
2025-01-19 16:10:18 +0100

    Created launchers for fine-tuning / training DINs on UKE Neurofibroma data

---
+++ launchers/train_dins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Train the SW-FastEdit model from scratch
+export TF_CPP_MIN_LOG_LEVEL=3
+export CUDA_VISIBLE_DEVICES=1
+
+python /home/gkolokolnikov/PhD_project/nf_segmentation_interactive/NFInteractiveSegmentationBenchmarking/model_code/DINs-Neurofibroma/entry3d.py \
+train with fold=1 \
+bs=4 \
+epochs=250 
