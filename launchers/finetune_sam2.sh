14e59d958ed1d76918749b071ebcdeca0cd7c2c8
GeorgBell <georgbell5@gmail.com>
2025-01-19 17:46:56 +0100

    Added files for finetuning SAM2 on the Neurofibroma dataset

---
+++ launchers/finetune_sam2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Feel free to update the number of used GPUs.
+# Also keep in mind that the paths in yaml files are hardcoded.
+# Please update date with respect to the relevant data and model location.
+# To train on a different fold please update the fold path in the yaml file.
+export CUDA_VISIBLE_DEVICES=0,1
+
+python /home/gkolokolnikov/PhD_project/nf_segmentation_interactive/NFInteractiveSegmentationBenchmarking/model_code/sam2-Neurofibroma/training/train.py \
+-c configs/sam2.1_training/sam2.1_hiera_b+_NF_finetune.yaml \
+--use-cluster 0 \
+--num-gpus 1
