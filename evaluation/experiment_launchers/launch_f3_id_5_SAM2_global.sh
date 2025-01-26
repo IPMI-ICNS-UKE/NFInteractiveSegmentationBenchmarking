55f4df8e85d7f9616faab9785f731afd3763bda4
GeorgBell <georgbell5@gmail.com>
2025-01-26 11:59:31 +0100

    Added launchers for evaluations

---
+++ evaluation/experiment_launchers/launch_f3_id_5_SAM2_global.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=1
+export PYTHONPATH=$(dirname "$(dirname "$(pwd)")"):$PYTHONPATH
+
+NETWORK="SAM2"
+EVAL_MODE="global_corrective"
+TEST_SET_IDS=(1 2 3)
+
+
+# Please choose parameters
+FOLD=1
+NUM_INTERACTIONS_PER_LESION=60
+
+
+echo "Running $NETWORK with evaluation mode: $EVAL_MODE"
+
+for TEST_SET_ID in "${TEST_SET_IDS[@]}"; do
+    echo "Processing test set $TEST_SET_ID..."
+    python ../pipelines/evaluation_pipeline.py --network_type $NETWORK --fold $FOLD --evaluation_mode $EVAL_MODE --num_lesions $NUM_LESIONS --num_interactions_per_lesion $NUM_INTERACTIONS_PER_LESION --test_set_id $TEST_SET_ID --save_predictions --use_gpu
+done
+
+echo "Completed $NETWORK with $EVAL_MODE"
