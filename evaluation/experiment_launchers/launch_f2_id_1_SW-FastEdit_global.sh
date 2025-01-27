9b10e3c9309d60846fc5b318bf29f11dc74d101c
GeorgBell <georgbell5@gmail.com>
2025-01-27 10:51:10 +0100

    Corrected an error in the global interaction evaluation pipeline launchers

--- evaluation/experiment_launchers/launch_f2_id_1_SW-FastEdit_global.sh
+++ evaluation/experiment_launchers/launch_f2_id_1_SW-FastEdit_global.sh
@@ -8,7 +8,7 @@ TEST_SET_IDS=(1 2 3)
 
 
 # Please choose parameters
-FOLD=1
+FOLD=2
 NUM_INTERACTIONS_PER_LESION=60
 
 
@@ -16,7 +16,7 @@ echo "Running $NETWORK with evaluation mode: $EVAL_MODE"
 
 for TEST_SET_ID in "${TEST_SET_IDS[@]}"; do
     echo "Processing test set $TEST_SET_ID..."
-    python ../pipelines/evaluation_pipeline.py --network_type $NETWORK --fold $FOLD --evaluation_mode $EVAL_MODE --num_lesions $NUM_LESIONS --num_interactions_per_lesion $NUM_INTERACTIONS_PER_LESION --test_set_id $TEST_SET_ID --save_predictions --use_gpu
+    python ../pipelines/evaluation_pipeline.py --network_type $NETWORK --fold $FOLD --evaluation_mode $EVAL_MODE --num_interactions_per_lesion $NUM_INTERACTIONS_PER_LESION --test_set_id $TEST_SET_ID --save_predictions --use_gpu
 done
 
 echo "Completed $NETWORK with $EVAL_MODE"
