3a74018b60a3543f05743a3759481355ee55ed26
GeorgBell <georgbell5@gmail.com>
2025-02-05 14:35:48 +0100

    Added documentation and README.MD description for the model_weights_finetuned folder

--- model_weights_finetuned/test_onnxruntime.py
+++ model_weights_finetuned/test_onnxruntime.py
@@ -2,23 +2,29 @@ import onnx
 import onnxruntime as ort
 import numpy as np
 
+
+"""
+This script loads a DINs model in ONNX format, verifies its integrity, 
+and performs inference using ONNX Runtime.
+"""
+# Display available execution providers
 print("Available Execution Providers:", ort.get_available_providers())
 
-# Path to the ONNX model
-onnx_model_path = "DINs/fold_1/checkpoint.onnx"  # Update path accordingly
+# Path to the ONNX model (update accordingly)
+onnx_model_path = "DINs/fold_1/checkpoint.onnx"
 
-# Load the ONNX model to check for integrity
+# Load the ONNX model and verify its integrity
 onnx_model = onnx.load(onnx_model_path)
 onnx.checker.check_model(onnx_model)
 print("ONNX model is valid.")
 
-# Create an inference session
+# Create an ONNX Runtime inference session
 providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
 ort_session = ort.InferenceSession(onnx_model_path, providers=providers)
 
 # Define the test input with expected shape (batch_size, depth, height, width, channels)
 batch_size = 1
-depth = 32  # Example depth value
+depth = 32 
 height = 960
 width = 320
 channels_image = 1
@@ -28,11 +34,13 @@ channels_guide = 2
 image_input = np.random.rand(batch_size, depth, height, width, channels_image).astype(np.float32)
 guide_input = np.random.rand(batch_size, depth, height, width, channels_guide).astype(np.float32)
 
-# Run inference
+# Prepare input dictionary for inference
 inputs = {
     "image": image_input,
     "guide": guide_input
 }
+
+# Run inference
 outputs = ort_session.run(None, inputs)
 
 # Print output shape and results
